24be3992f6d9ded283877a9a3a1374166e1968ad
gedeonagmas <gedeonagmas2580@gmail.com>
2024-10-26 07:10:14 +0300

    updated

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   trailingSlash: true,
   images: {
-    domains: ["127.0.0.1"],
-    //   domains: ["api.gsbiomedical.com"],
+    // domains: ["127.0.0.1"],
+    domains: ["api.gsbiomedical.com"],
   },
   eslint: {
     ignoreDuringBuilds: true, // Ignores all ESLint warnings/errors during builds
